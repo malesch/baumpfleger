cf9dae9c6c5af518d34859ac21ebf81d0d9b00e9
Marcus Spiegel <malesch@gmail.com>
2016-12-29 19:16:03 +0100

    Major refactoring
     - Get rid of unnessary complicated indexed tree configuration
     - Fix cursor usage (assign on creation of the component)

--- src/baumpfleger/tree.cljs
+++ src/baumpfleger/tree.cljs
@@ -4,56 +4,52 @@
 (defn branch? [node]
   (contains? node :children))
 
-(defn- index-tree*
-  [tree]
-  (let [{:keys [id children]} tree]
-    (if (branch? tree)
-      [id (assoc tree :children (into {} (map index-tree* children)))]
-      [id tree])))
-
-(defn index-tree
-  "Transform the tree description into an indexed form where every node
-  can be accessed by an ID lookup. This structure simplifies later processing."
-  [tree]
-  (into {} [(index-tree* tree)]))
-
-
 (defmulti ->node (fn [tc] (or (:type @tc) :branch)))
 
-(defmethod ->node :branch [tc]
-  (fn [tc]
-    (let [{:keys [id label children open?] :as inode} @tc]
-      ^{:key inode}
-      [:ul
-       [:li.branch [:span {:on-click #(swap! tc update :open? not)
-                           :class (if open? "open" "closed")} label]]
-       [:ul {:style {:display (if (:open? @tc) "display" "none")}}
-        (doall
-          (for [child children]
-            ^{:key child}
-            [->node (r/cursor tc [:children (first child)])]))]])))
+(defmethod ->node :branch [node-cursor]
+  (let [nc node-cursor]
+    (fn []
+      (let [{:keys [id label children open?] :as node} @nc]
+        ^{:key node}
+        [:li.branch
+         [:span {:on-click #(swap! nc update :open? not)
+                 :class    (if open? "open" "closed")} label]
+         [:ul {:style {:display (if open? "display" "none")}}
+          (for [idx (range (count children))]
+            ^{:key idx}
+            [->node (r/cursor nc [:children idx])])]]))))
 
-(defmethod ->node :float [tc]
-  (fn [tc]
-    (let [{:keys [id label value] :as inode} @tc]
-      ^{:key inode}
-      [:li.input
-       [:div.input-float
-        [:label label]
-        [:input {:type "text" :value value}]]])))
+(defmethod ->node :float [node-cursor]
+  (let [nc node-cursor]
+    (fn []
+      (let [{:keys [id label value] :as node} @nc]
+        ^{:key node}
+        [:li.input
+         [:div.input-float
+          [:label label]
+          [:input {:type      "text"
+                   :default   ""
+                   :value     (:value @nc)
+                   :on-change #(let [v (-> % .-target .-value)]
+                                 (swap! nc assoc :value v))}]]]))))
 
-(defmethod ->node :int [tc]
-  (fn [tc]
-    (let [{:keys [id label value] :as inode} @tc]
-      ^{:key inode}
-      [:li.input
-       [:div.input-int
-        [:label label]
-        [:input {:type "text" :value value}]]])))
+(defmethod ->node :int [node-cursor]
+  (let [nc node-cursor]
+    (fn []
+      (let [{:keys [id label value] :as node} @nc]
+        ^{:key node}
+        [:li.input
+         [:div.input-int
+          [:label label]
+          [:input {:type      "text"
+                   :default   ""
+                   :value     (:value @nc)
+                   :on-change #(let [v (-> % .-target .-value)]
+                                 (swap! nc assoc :value v))}]]]))))
 
-(defn render-tree [itree]
-  [:div.baumpfleger
-   (doall
-     (for [inode @itree]
-       (let [[idx _] inode]
-         ^{:key inode} [->node (r/cursor itree [idx])])))])
+(defn render-tree [root-cursor]
+  (let [rc root-cursor]
+    (fn []
+      [:div.baumpfleger
+       [:ul
+        [->node rc]]])))
